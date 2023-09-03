af5523c5eddb76e7a98c0c1728c5436c409534c7
totegamma <github@gammalab.net>
2023-09-03 22:21:25 +0900

    small fix

--- cmd/activitypub/main.go
+++ cmd/activitypub/main.go
@@ -144,6 +144,7 @@ func main() {
 	ap := e.Group("/ap")
 	ap.GET("/nodeinfo/2.0", activitypubHandler.NodeInfo)
 	ap.GET("/acct/:id", activitypubHandler.User)
+	ap.GET("/key/:id", activitypubHandler.User) // for backward compatibility
 	ap.POST("/acct/:id/inbox", activitypubHandler.Inbox)
 	ap.POST("/acct/:id/outbox", activitypubHandler.PrintRequest)
 	ap.GET("/note/:id", activitypubHandler.Note)
